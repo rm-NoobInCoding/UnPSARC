e8cb77dec7dd2d6d51a87f02d727abff20aea3ac
rezamms <srzrezamms@gmail.com>
2022-10-20 18:13:53 +0330

    Support Zlib Archives

---
+++ Zlib.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ionic.Zlib;
+using System.IO;
+
+namespace UnPSARC
+{
+    public class Zlib
+    {
+        public static byte[] Decompress(byte[] Data , int decompsize)
+        {
+            MemoryStream input = new MemoryStream(Data);
+            MemoryStream output = new MemoryStream();
+            using (ZlibStream zlib = new ZlibStream(input, CompressionMode.Decompress))
+            {
+                zlib.CopyTo(output);
+            }
+
+            return output.ToArray();
+        }
+    }
+}
