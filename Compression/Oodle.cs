ed31a94ad1c9eaa715dfabafa3c41f02a73a5b42
rezamms <srzrezamms@gmail.com>
2022-10-22 21:33:57 +0330

    Remove useless codes

--- Compression/Oodle.cs
+++ Compression/Oodle.cs
@@ -7,59 +7,11 @@ using System.Runtime.InteropServices;
 namespace UnPSARC
 {
 
-    public enum OodleFormat : uint
-    {
-        LZH,
-        LZHLW,
-        LZNIB,
-        None,
-        LZB16,
-        LZBLW,
-        LZA,
-        LZNA,
-        Kraken,
-        Mermaid,
-        BitKnit,
-        Selkie,
-        Akkorokamui
-    }
-    public enum OodleCompressionLevel : ulong
-    {
-        None,
-        SuperFast,
-        VeryFast,
-        Fast,
-        Normal,
-        Optimal1,
-        Optimal2,
-        Optimal3,
-        Optimal4,
-        Optimal5
-    }
-
     public static class Oodle
     {
 
         [DllImport("oo2core_9_win64.dll")]
-        public static extern int GetCompressedBufferSizeNeeded(long rawSize);
-        [DllImport("oo2core_9_win64.dll")]
-        private static extern int OodleLZ_Compress(OodleFormat format, byte[] buffer, long bufferSize, byte[] outputBuffer, OodleCompressionLevel level, uint unused1, uint unused2, uint unused3);
-
-        [DllImport("oo2core_9_win64.dll")]
         private static extern int OodleLZ_Decompress(byte[] buffer, long bufferSize, byte[] outputBuffer, long outputBufferSize, uint a, uint b, ulong c, uint d, uint e, uint f, uint g, uint h, uint i, uint threadModule);
-
-        public static byte[] Compress(byte[] buffer, int size, OodleFormat format, OodleCompressionLevel level)
-        {
-            uint compressedBufferSize = GetCompressionBound((uint)size);
-            byte[] compressedBuffer = new byte[compressedBufferSize];
-
-            int compressedCount = OodleLZ_Compress(format, buffer, size, compressedBuffer, level, 0, 0, 0);
-
-            byte[] outputBuffer = new byte[compressedCount];
-            Buffer.BlockCopy(compressedBuffer, 0, outputBuffer, 0, compressedCount);
-
-            return outputBuffer;
-        }
 
         public static byte[] Decompress(byte[] buffer, int uncompressedSize)
         {
@@ -74,34 +26,6 @@ namespace UnPSARC
                 return decompressedBuffer.Take(decompressedCount).ToArray();
             else
                 throw new Exception("There was an error while decompressing");
-        }
-
-        private static uint GetCompressionBound(uint bufferSize)
-        {
-            return bufferSize + 274 * ((bufferSize + 0x3FFFF) / 0x40000);
-        }
-
-        /*private static uint GetDecompressionBound(uint bufferSize)
-        {
-            uint v2 = bufferSize + 272 + 0;
-            uint v3 = (bufferSize + 0x3FFFF) / 0x40000;
-            if (bufferSize + 16731 + 2 * v3 < v2)
-            {
-                v2 = bufferSize + 16731 + 2 * v3;
-            }
-            return v2;
-        }*/
-        /// <summary>
-        /// Gets the max buffer size needed for oodle compression
-        /// </summary>
-        /// <param name="count"></param>
-        /// <returns></returns>
-        public static int GetCompressedBufferSizeNeeded(int count)
-        {
-            var n = (((count + 0x3ffff + ((uint)((count + 0x3ffff) >> 0x3f) & 0x3ffff))
-                     >> 0x12) * 0x112) + count;
-            //var n  = OodleNative.GetCompressedBufferSizeNeeded((long)count);
-            return (int)n;
         }
     }
 }
