01b72df9ebafb383acf314b6e20840f003dae5c1
RezaMirMohammadSadeghi <srzrezamms@gmail.com>
2024-05-20 13:42:19 +0330

    Updated unpacker to write unpacked buffer into a stream (insted of byte array)

--- Archive.cs
+++ Archive.cs
@@ -1,6 +1,7 @@
 ﻿using Gibbed.IO;
 using System;
 using System.IO;
+using UnPSARC.Helpers;
 
 namespace UnPSARC
 {
@@ -12,6 +13,7 @@ namespace UnPSARC
             Stream Reader = ArchiveRaw;
             PSARC Psarc = new PSARC(Reader);
             Psarc.Read();
+            int FailedFiles = 0;
 
             for (int i = 0; i < Psarc.FilesCount; i++)
             {
@@ -21,24 +23,35 @@ namespace UnPSARC
                 if (!Psarc.FileNames.ContainsKey(BitConverter.ToString(ThisEntry.HashNames))) throw new Exception("Archive Contains a hash which is not in filenames table:");
                 string FileName = Psarc.FileNames[BitConverter.ToString(ThisEntry.HashNames)].Replace("/", "\\");
                 if (FileName.StartsWith("\\")) FileName = FileName.Remove(0, 1);
-                byte[] UnpackedData = TryUnpack(Psarc.Reader, Psarc.Entries[i], Psarc.ZSizes, Psarc.BlockSize, Psarc.CompressionType);
-                IOHelper.CheckFolderExists(FileName, Folder);
-                File.WriteAllBytes(@"\\?\" + Path.Combine(Folder, FileName), UnpackedData);
-                Console.WriteLine("[" + i + "] " + FileName + " Exported!");
+                try
+                {
+                    IOHelper.CheckFolderExists(FileName, Folder);
+                    TryUnpack(Psarc.Reader, out HugeMemoryStream FileWriter, Psarc.Entries[i], Psarc.ZSizes, Psarc.BlockSize, Psarc.CompressionType);
+                    Stream fileHandle = File.Open(@"\\?\" + Path.Combine(Folder, FileName), FileMode.Create, FileAccess.Write);
+                    FileWriter.CopyTo(fileHandle);
+                    fileHandle.Close();
+                    Console.WriteLine("[" + i + "] " + FileName + " Exported!");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("[" + i + "] ---" + FileName + " Cannot Exported! Error:" + ex.Message);
+                    FailedFiles++;
+                }
+
+
 
             }
-            Console.WriteLine("Unpacking done! | " + Psarc.FilesCount + " File Exported");
-            //Console.WriteLine("Press Any Key To Continue...");
-            //Console.ReadKey();
+            Console.WriteLine($"Unpacking done! | {Psarc.FilesCount - FailedFiles} of {Psarc.FilesCount} Files Exported");
 
         }
 
-        public static byte[] TryUnpack(Stream Reader, TEntry ThisEntry, TZSize[] ZSizes, int BlockSize, string CompressionType)
+        public static void TryUnpack(Stream Reader,out HugeMemoryStream Writer, TEntry ThisEntry, TZSize[] ZSizes, int BlockSize, string CompressionType)
         {
             long RemainingSize = ThisEntry.UncompressedSize;
             int ZSizeIndex = ThisEntry.ZSizeIndex;
             long BlockOffset = ThisEntry.Offset;
-            Stream MEMORY_FILE = new MemoryStream();
+            Stream MEMORY_FILE = new HugeMemoryStream();
 
             while (MEMORY_FILE.Length < ThisEntry.UncompressedSize)
             {
@@ -65,7 +78,8 @@ namespace UnPSARC
                 BlockOffset += (uint)CompressedSize;
                 RemainingSize -= BlockSize;
             }
-            return MEMORY_FILE.ToByteArray();
+
+            Writer = (HugeMemoryStream)MEMORY_FILE;
         }
 
     }
