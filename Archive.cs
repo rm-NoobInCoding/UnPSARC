f537d250b15167d2a3e8b541174b9c46c79a7000
RezaMirMohammadSadeghi <srzrezamms@gmail.com>
2024-06-06 15:22:46 +0330

    Show unknown hash in error

--- Archive.cs
+++ Archive.cs
@@ -18,15 +18,24 @@ namespace UnPSARC
             for (int i = 0; i < Psarc.FilesCount; i++)
             {
                 TEntry ThisEntry = Psarc.Entries[i];
-                if (ThisEntry.Offset == 0) continue;
 
-                if (!Psarc.FileNames.ContainsKey(BitConverter.ToString(ThisEntry.HashNames))) throw new Exception("Archive Contains a hash which is not in filenames table:");
-                string FileName = Psarc.FileNames[BitConverter.ToString(ThisEntry.HashNames)].Replace("/", "\\");
-                if (FileName.StartsWith("\\")) FileName = FileName.Remove(0, 1);
+                if (ThisEntry.Offset == 0)
+                    continue;
+
+                string filenameHash = BitConverter.ToString(ThisEntry.HashNames);
+
+                if (!Psarc.FileNames.ContainsKey(filenameHash))
+                    throw new Exception("Archive Contains a hash which is not in filenames table: " + filenameHash);
+
+                string FileName = Psarc.FileNames[filenameHash].Replace("/", "\\");
+
+                if (FileName.StartsWith("\\"))
+                    FileName = FileName.Remove(0, 1);
+
                 try
                 {
-                    IOHelper.CheckFolderExists(FileName, Folder);
                     TryUnpack(Psarc.Reader, out HugeMemoryStream FileWriter, Psarc.Entries[i], Psarc.ZSizes, Psarc.BlockSize, Psarc.CompressionType);
+                    IOHelper.CheckFolderExists(FileName, Folder);
                     Stream fileHandle = File.Open(@"\\?\" + Path.Combine(Folder, FileName), FileMode.Create, FileAccess.Write);
                     FileWriter.CopyTo(fileHandle);
                     fileHandle.Close();
@@ -41,14 +50,13 @@ namespace UnPSARC
                     FailedFiles++;
                 }
 
-
-
             }
+
             Console.WriteLine($"Unpacking done! | {Psarc.FilesCount - FailedFiles} of {Psarc.FilesCount} Files Exported");
 
         }
 
-        public static void TryUnpack(Stream Reader,out HugeMemoryStream Writer, TEntry ThisEntry, TZSize[] ZSizes, int BlockSize, string CompressionType)
+        public static void TryUnpack(Stream Reader, out HugeMemoryStream Writer, TEntry ThisEntry, TZSize[] ZSizes, int BlockSize, string CompressionType)
         {
             long RemainingSize = ThisEntry.UncompressedSize;
             int ZSizeIndex = ThisEntry.ZSizeIndex;
@@ -58,28 +66,21 @@ namespace UnPSARC
             while (MEMORY_FILE.Length < ThisEntry.UncompressedSize)
             {
                 int CompressedSize = ZSizes[ZSizeIndex++].ZSize;
-                if (CompressedSize == 0) CompressedSize = BlockSize;
+
+                if (CompressedSize == 0)
+                    CompressedSize = BlockSize;
+
                 if (CompressedSize == ThisEntry.UncompressedSize)
-                {
-
                     MEMORY_FILE.WriteBytes(Reader.ReadAtOffset(BlockOffset, ThisEntry.UncompressedSize));
-
-                }
                 else if (RemainingSize < BlockSize || CompressedSize == BlockSize)
-                {
-
                     MEMORY_FILE.WriteBytes(Reader.ReadAtOffset(BlockOffset, RemainingSize, CompressedSize, CompressionType));
-
-                }
                 else
-                {
-
                     MEMORY_FILE.WriteBytes(Reader.ReadAtOffset(BlockOffset, BlockSize, CompressedSize, CompressionType));
 
-                }
                 BlockOffset += (uint)CompressedSize;
                 RemainingSize -= BlockSize;
             }
+
             MEMORY_FILE.Position = 0;
             Writer = (HugeMemoryStream)MEMORY_FILE;
         }
